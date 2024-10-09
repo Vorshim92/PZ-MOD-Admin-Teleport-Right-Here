5783866eec288fbd039175e4a1e794f818a02e53
Stefano Scalfari <vorshim92@gmail.com>
2024-10-09 17:24:02 +0200

    first commit

---
+++ Contents/mods/Admin Teleport Right Here/media/lua/client/adminteleport.lua
@@ -0,0 +1,60 @@
+-- Function to teleport the admin
+local function tpToCoord(admin, x, y, z)
+    print("Teleporting to:", x, y, z)
+    admin:setX(x)
+    admin:setY(y)
+    admin:setZ(z)
+    admin:setLx(x)
+    admin:setLy(y)
+    admin:setLz(z)
+end
+
+-- Save the original function
+-- local original_AdmindoMenu = AdminContextMenu.doMenu
+
+local function AdminTeleportContextMenu(player, context, worldobjects, test)
+	-- original_AdmindoMenu(player, context, worldobjects, test)
+	local admin = getSpecificPlayer(player)
+    if not admin or admin:getAccessLevel() ~= "Admin" then
+        return
+    end
+
+	-- if test then return ISWorldObjectContextMenu.setTest() end
+
+	local square;
+	
+	for i,v in ipairs(worldobjects) do
+		square = v:getSquare();
+		break;
+	end
+
+	local x,y,z = square:getX(), square:getY(), square:getZ();
+	-- local sqTag = x .. "x" .. y .. "x" .. z;
+
+	-- Get the "Tools" submenu
+	-- local debugOption = context:getOptionFromName("Tools")
+	-- if debugOption then
+	-- 	print("test.lua: debugOption exists")
+	-- 	local submenu = context:getSubMenu(debugOption.subOption)
+	-- 	if submenu then
+	-- 		print("test.lua: submenu exists")
+	-- 		-- Use addOptionOnTop to add the option at the first position
+	-- 		submenu:addOptionOnTop("Teleport here", admin, tpToCoord, x, y, z)
+	-- 		-- if submenu:getOptionFromName("Teleport") then
+	-- 		-- submenu:insertOptionBefore("Teleport", "Teleport here", admin, tpToCoord, x, y, z)
+	-- 		-- end
+	-- 		-- if submenu:getOptionFromName("Spawn Vehicle") then
+	-- 		-- submenu:insertOptionAfter("Spawn Vehicle", "Teleport here", admin, tpToCoord, x, y, z)
+	-- 		-- end
+
+	-- 	else
+	-- 		print("test.lua: submenu not found")
+	-- 	end
+	-- else
+	-- 	print("test.lua: Tools option not found")
+	-- end
+	context:addOptionOnTop("Teleport Here", admin, tpToCoord, x, y, z)
+
+end
+
+Events.OnFillWorldObjectContextMenu.Add(AdminTeleportContextMenu)
